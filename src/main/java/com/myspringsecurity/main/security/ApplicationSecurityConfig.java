b926c31306a7077b13ba9af56ac29a922c06d896
zavlagas <zavlagas@hotmail.com>
2021-01-31 05:27:03 +0200

    My Spring Security Configurations

--- src/main/java/com/myspringsecurity/main/security/ApplicationSecurityConfig.java
+++ src/main/java/com/myspringsecurity/main/security/ApplicationSecurityConfig.java
@@ -24,6 +24,7 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
     }
 
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
